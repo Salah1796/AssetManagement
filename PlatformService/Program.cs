1949e0a8e60b80c8f8f14707ac7a425b69309301
Salah <s.ali@apex.ps>
2025-10-18 20:51:17 +0300

    Added SQL SERVER

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -9,9 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services
-    .AddDbContext<AppDbContext>(opt =>
-    opt.UseInMemoryDatabase("InMem"));
+
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine($" --- UseSqlServer {builder.Configuration.GetConnectionString("PlatformsConn")}  ---- ");
+    builder.Services
+        .AddDbContext<AppDbContext>(opt 
+        => opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine($" ---- UseInMemoryDatabase --- ");
+    builder.Services
+        .AddDbContext<AppDbContext>(opt => 
+        opt.UseInMemoryDatabase("InMem"));
+}
+
 
 builder.Services.AddAutoMapper(typeof(PlatformsProfile));
 
@@ -22,7 +35,6 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
 });
-
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 var app = builder.Build();
@@ -34,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlatformService v1"));
 }
+
 
 app.UseHttpsRedirection();
 
